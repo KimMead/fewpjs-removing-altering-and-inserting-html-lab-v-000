40c1b67867960498470d55d5a557789c047a475c
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-11-13 01:15:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your code here!
 document.getElementById('main').remove()
 
-let newHeader = document.createElement('h1'); 
+let newHeader = document.createElement('h1');
 newHeader.id = 'victory'
 newHeader.innerHTML = 'is the champion'
-document.body.appendChild(newHeader); 
+document.body.appendChild(newHeader);
